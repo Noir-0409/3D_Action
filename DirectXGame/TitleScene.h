42b7585229bd09d1d549b8c88dfedc413d4ba301
Noir-0409 <146914778+Noir-0409@users.noreply.github.com>
2025-09-29 16:01:20 +0900

    TitleScene追加

---
+++ DirectXGame/TitleScene.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "KamataEngine.h"
+
+using namespace KamataEngine;
+
+class TitleScene {
+
+public:
+
+~TitleScene();
+
+void Initialize();
+
+void Update();
+
+void Draw();
+
+private:
+
+
+
+};
