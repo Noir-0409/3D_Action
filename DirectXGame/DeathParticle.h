1fe368294ae4c24cde8b14abd16840a4a66b50c9
Noir-0409 <146914778+Noir-0409@users.noreply.github.com>
2025-10-15 21:06:30 +0900

    パーティクル追加

--- DirectXGame/DeathParticle.h
+++ DirectXGame/DeathParticle.h
@@ -22,4 +22,10 @@ static inline const uint32_t kNumParticles = 8;
 
 std::array<WorldTransform, kNumParticles> worldTransforms_;
 
+static inline const float kDuration = 1.0f;
+
+static inline const float kSpeed = 0.1f;
+
+static inline const float kAngleUint = 2.0f * 3.14f / 8.0f;
+
 };
