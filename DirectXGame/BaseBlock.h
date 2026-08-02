d58656ed538a356a7e09e1fe0d32abd5d42babab
Noir-0409 <146914778+Noir-0409@users.noreply.github.com>
2026-08-02 23:29:06 +0900

    終了

--- DirectXGame/BaseBlock.h
+++ DirectXGame/BaseBlock.h
@@ -3,23 +3,23 @@
 #include "3D/Model.h"
 #include "CameraController.h"
 #include "KamataEngine.h"
-#include <memory> // ★ std::unique_ptr を使うために追加
+#include <memory>
+
+namespace KamataEngine { // ★ namespace の開始
 
 class BaseBlock {
 protected:
-	KamataEngine::Model* model_ = nullptr;
-	// ⭕ 生ポインタから std::unique_ptr に変更
-	std::unique_ptr<KamataEngine::WorldTransform> worldTransform_ = nullptr;
+	Model* model_ = nullptr;
+	std::unique_ptr<WorldTransform> worldTransform_ = nullptr;
 
 public:
-	// ⭕ 引数で受け取る型を std::unique_ptr に合わせ、std::move で所有権を受け取る
-	BaseBlock(KamataEngine::Model* model, std::unique_ptr<KamataEngine::WorldTransform> wt) : model_(model), worldTransform_(std::move(wt)) {}
+	BaseBlock(Model* model, std::unique_ptr<WorldTransform> wt) : model_(model), worldTransform_(std::move(wt)) {}
 
-	// ⭕ 仮想デストラクタ（スマートポインタが自動解放するため、deleteは完全に消去！）
 	virtual ~BaseBlock() = default;
 
-	virtual void Draw(const KamataEngine::Camera& camera) = 0;
+	virtual void Draw(const Camera& camera) = 0;
 
-	// ⭕ 外部（GameSceneなど）がポインタとして扱えるように、.get() を使って生ポインタとして返す
-	KamataEngine::WorldTransform* GetWorldTransform() const { return worldTransform_.get(); }
+	WorldTransform* GetWorldTransform() const { return worldTransform_.get(); }
 };
+
+} // namespace KamataEngine
